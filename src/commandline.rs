21e4c1b82c95014e6fe2301be8cfbfe0f63b69c1
jutn-dev <outo.outo22@gmail.com>
2025-07-19 16:56:10 +0300

    added support for moving the cursor

--- src/commandline.rs
+++ src/commandline.rs
@@ -15,6 +15,12 @@ use crate::{ConsoleCommandEntered, ConsoleConfiguration, ConsoleState};
 #[derive(Resource, Debug, Clone)]
 pub(crate) struct CommandlineState {
     pub(crate) scrollbacks_printed: usize,
+    ///cursor_position is the amout of inexes in the string not the amout of chars
+    pub(crate) cursor_position: usize,
+
+    //TODO
+    //config options move some where else
+
     //Why use crossterm `KeyCode` instead of bevy's?
     //All the terminal input is hadeled by crossterm so it's simpler to use crossterm's `KeyCode`s.
     pub exit_key: Vec<crossterm::event::KeyCode>,
@@ -24,6 +30,7 @@ impl Default for CommandlineState {
     fn default() -> Self {
         CommandlineState {
             scrollbacks_printed: 0,
+            cursor_position: 0,
             exit_key: vec![
                 crossterm::event::KeyCode::Modifier(ModifierKeyCode::LeftControl),
                 crossterm::event::KeyCode::Char('c'),
@@ -45,6 +52,7 @@ pub(crate) fn cleanup_commandline(mut exit_event: EventReader<AppExit>) {
 }
 pub(crate) fn commandline(
     mut console_state: ResMut<ConsoleState>,
+    mut commandline_state: ResMut<CommandlineState>,
     mut exit_event: EventWriter<AppExit>,
     mut command_entered: EventWriter<'_, ConsoleCommandEntered>,
     config: Res<ConsoleConfiguration>,
@@ -53,12 +61,47 @@ pub(crate) fn commandline(
         let events = crossterm::event::read().unwrap();
         if let crossterm::event::Event::Key(key) = events {
             if let crossterm::event::KeyCode::Char(c) = key.code {
-                console_state.buf.push(c);
+                //finds the correct position to insert the char
+                let mut index = 0;
+                if commandline_state.cursor_position != 0 {
+                    //get char and its staring index
+                    index = match console_state
+                        .buf
+                        .char_indices()
+                        .nth(commandline_state.cursor_position - 1)
+                    {
+                        None => 0,
+                        //add last char's len to get the correct position
+                        Some(char) => char.0 + char.1.len_utf8(),
+                    };
+                }
+                console_state.buf.insert(index, c);
+                commandline_state.cursor_position += 1;
             }
+
             if key.code == crossterm::event::KeyCode::Backspace {
-                console_state.buf.pop();
+                if commandline_state.cursor_position < 1 {
+                    continue;
+                }
+                console_state
+                    .buf
+                    .remove(commandline_state.cursor_position - 1);
+                commandline_state.cursor_position -= 1;
+            }
+            if key.code == crossterm::event::KeyCode::Left {
+                if commandline_state.cursor_position == 0 {
+                    continue;
+                }
+                commandline_state.cursor_position -= 1;
+            }
+            if key.code == crossterm::event::KeyCode::Right {
+                if commandline_state.cursor_position >= console_state.buf.len() {
+                    continue;
+                }
+                commandline_state.cursor_position += 1;
             }
             if key.code == crossterm::event::KeyCode::Enter {
+                commandline_state.cursor_position = 0;
                 handle_enter(&mut console_state, &config, &mut command_entered);
             }
             if key.code == crossterm::event::KeyCode::Esc {
@@ -76,8 +119,8 @@ pub(crate) fn update_terminal(
 ) {
     let mut stdout = std::io::stdout();
 
-    redraw_commandline(&console_state, &config);
-    
+    redraw_commandline(&commandline_state, &console_state, &config);
+
     for line in console_state
         .scrollback
         .iter()
@@ -90,17 +133,22 @@ pub(crate) fn update_terminal(
 }
 
 ///redraws the line where command is inputed
-fn redraw_commandline(state: &ConsoleState, config: &ConsoleConfiguration) {
+fn redraw_commandline(
+    commandline_state: &CommandlineState,
+    console_state: &ConsoleState,
+    config: &ConsoleConfiguration,
+) {
     execute!(std::io::stdout(), Clear(ClearType::CurrentLine)).unwrap();
     execute!(std::io::stdout(), MoveToColumn(0)).unwrap();
     execute!(
         std::io::stdout(),
-        Print(format!("{}{}", config.symbol, state.buf))
+        Print(format!("{}{}", config.symbol, console_state.buf))
     )
     .unwrap();
+
     execute!(
         std::io::stdout(),
-        MoveToColumn((config.symbol.chars().count() + state.buf.chars().count()) as u16)
+        MoveToColumn((config.symbol.chars().count() + commandline_state.cursor_position) as u16)
     )
     .unwrap();
 }
@@ -111,8 +159,8 @@ fn handle_enter(
     command_entered: &mut EventWriter<'_, ConsoleCommandEntered>,
 ) {
     execute!(std::io::stdout(), Print("\r\n",)).unwrap();
-    
-    //this code is almost the same as the console's 
+
+    //this code is almost the same as the console's
     if state.buf.trim().is_empty() {
         state.scrollback.push(String::new());
     } else {
